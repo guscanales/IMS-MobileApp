d9499309b9a983932eb3870ddf5c46598e42037d
guscanales <gustavo.canaleslopez@gmail.com>
2016-01-16 01:35:41 -0300

    Initial Commit

---
+++ www/js/controllers.js
@@ -0,0 +1,13 @@
+angular.module('app.controllers', [])
+
+.controller('ingresoCtrl', function($scope) {
+
+})
+
+.controller('registroCtrl', function($scope) {
+
+})
+
+.controller('inAppCtrl', function($scope) {
+  window.open('http://www.industrialms.cl','_self'); 
+})
---
+++ www/js/routes.js
@@ -0,0 +1,45 @@
+angular.module('app.routes', [])
+
+.config(function($stateProvider, $urlRouterProvider) {
+
+  // Ionic uses AngularUI Router which uses the concept of states
+  // Learn more here: https://github.com/angular-ui/ui-router
+  // Set up the various states which the app can be in.
+  // Each state's controller can be found in controllers.js
+  $stateProvider
+    
+      
+        
+    .state('ingreso', {
+      url: '/login',
+      templateUrl: 'templates/ingreso.html',
+      controller: 'ingresoCtrl'
+    })
+        
+      
+    
+      
+        
+    .state('registro', {
+      url: '/register',
+      templateUrl: 'templates/registro.html',
+      controller: 'registroCtrl'
+    })
+        
+      
+    
+      
+        
+    .state('inApp', {
+      url: '/inapp',
+      templateUrl: 'templates/inApp.html',
+      controller: 'inAppCtrl'
+    })
+        
+      
+    ;
+
+  // if none of the above states are matched, use this as the fallback
+  $urlRouterProvider.otherwise('/inapp');
+
+});
